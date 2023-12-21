9fc73499ec58d7c32d671f0230cbd7aeadfa3d66
reginag99 <regina-maria@live.se>
2023-12-21 07:11:32 +0100

    Updated files

--- KAA052 - Mathematical modelling in chemical engineering/test_variation.m
+++ KAA052 - Mathematical modelling in chemical engineering/test_variation.m
@@ -9,18 +9,16 @@
 %Lighter colour = low values of constant
 %Darker colour = high values of constant
 
-
 clear; clc; clf; close  all
-T_water0 = 90+273.15;
-m0 = 150*10^-3;
-T0 = [T_water0,m0]; %startvalues for the ode
-t_span =[0:60000];
-t_h = t_span(end)/3600
+S=readtable("small_beaker_3.txt"); %change is comparing with another set of data
+T_water0 = S{1,2}+273.15;
+m0 = S{1,3}*10^-3;
 
-selctiontype = [{'T_{air}'}, {'r_{inner}'}, {'r_{outer}'}, {'L'},{'k'},{'epsilon'}];%constants that can change. Choose one of them with 'Select'
-Select = 5; %The constant that will vary. 1 = T_air, 2 = r_inner ect
-I = 4; %Number of different values of constant you want to include in the plot
-
+T0 = [T_water0,m0]; %inital values for the ode
+t_span =linspace(0,2000);% S{:,1};
+selctiontype = [{'T_{air}'}, {'r_{inner}'}, {'r_{outer}'}, {'L'}, {'epsilon'},{'k'}];%constants that can change. Choose one of them with 'Select'
+Select = 4; %The constant that will vary. 1 = T_air, 2 = r_inner ect
+I = 7; %Number of different values of constant you want to include in the plot. OBS! If I =m, the plot will have m +1 iterations and lines as it will also plot iter = 0.
 
 figure("Name", "Variatin of constant  " + selctiontype(Select))
 subplot(2,1,1)
@@ -32,29 +30,23 @@ xlabel('Time, [s]','FontSize',12,'FontWeight','bold')
 ylabel('Mass, [g]','FontSize',12,'FontWeight','bold')
 hold on
 
-fontsize(16,"points")
-
 Green_light = [60,200,160]/255;
 Green_dark = [10,130,90]/255;
-
 Orange_light = [255,130,30]/255;
 Orange_dark = [180,60,0]/255;
-
-
-
 
 for iter = 0:I
     Green_grad = @(iter) Green_light + (Green_dark - Green_light)/I*iter;
     Orange_grad = @(iter) Orange_light + (Orange_dark - Orange_light)/I*iter;
 
     It_opt = [Select,I,iter];
-    [t,T] = ode45(@(t,T) dT_dt_mug(t,T,It_opt,m0),t_span,T0);
+    [t,T] = ode45(@(t,T) dT_dt_RG2(t,T,It_opt,m0),t_span,T0);
 
     %Temp plots
     subplot(2,1,1)
     plot(t,T(:,1),'Color',Green_grad(iter),'linewidth',1); hold on;
     title(['Variation analysis for ', selctiontype{Select}, ', heat transfer'])
-    yline(273.15+50, 'r--', LineWidth=1)  
+   % yline(273.15+50, 'r--', LineWidth=1)  
     %mass plots
     subplot(2,1,2)
     plot(t,T(:,2),'Color',Orange_grad(iter),'linewidth',1); hold on;
